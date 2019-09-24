ee039ac26f74a3ce6f8b717266b46ae5411ee5e5
Vadym Bobrenok <vadym.bobrenok@holoplot.com>
2019-09-24 14:23:43 +0200

    Fix login redirect, disable password reset

--- addons/src/urls.py
+++ addons/src/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.shortcuts import redirect
 from django.conf.urls import include, url
 
 from tcms.kiwi_auth import views
@@ -6,7 +7,8 @@ from tcms.urls import urlpatterns
 
 
 if hasattr(settings, 'DISABLE_REGISTRATION') and settings.DISABLE_REGISTRATION:
-    urlpatterns.insert(0, url(r'^accounts/register/$', views.LoginViewWithCustomTemplate.as_view(), name='tcms-register'))
+    urlpatterns.insert(0, url(r'^accounts/register/$', lambda request: redirect('/', permanent=True)))
+    urlpatterns.insert(0, url(r'^accounts/passwordreset/$', lambda request: redirect('/', permanent=True)))
 
 urlpatterns += [
     url('', include('social_django.urls', namespace='social')),
